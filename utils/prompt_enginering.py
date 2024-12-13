83c8dab50f37d34d8238141fdca4dfecfd2e584b
Jorge Ballester <jorge.ballcor@gmail.com>
2024-12-13 15:09:35 +0100

    fix: prompt template

--- utils/prompt_enginering.py
+++ utils/prompt_enginering.py
@@ -3,9 +3,20 @@ from langchain_core.prompts import ChatPromptTemplate
 
 def define_prompt_template():
     prompt=ChatPromptTemplate.from_messages(
-        [
-            ("system","You are a helpful massistant . Please  repsonse to the user queries"),
-            ("user","Question:{question}")
-        ]
+        prompt = ChatPromptTemplate.from_messages([
+        ("system", 
+         """You are an expert conversational assistant designed to provide accurate and concise answers. 
+         Use the following context to respond to user queries. If the context does not provide enough information, 
+         say "I’m sorry, I do not have enough information to answer that." 
+         Ensure your responses are:
+         - Clear and easy to understand.
+         - Limited to three sentences unless explicitly requested otherwise.
+         - Factually correct, avoiding speculation or assumptions.
+         """),
+        ("user", 
+         """User Question:\n{question}"""),
+        ("assistant", 
+         """Response:""")
+    ])
     )
     return prompt
